3341db0ed325612c9d7f03c091db3948d8b33d52
imYijun <imyijun@foxmail.com>
2018-06-26 16:58:17 +0800

    modify app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 //app.js
 App({
-    globalData: {
-        username:'User',
-        phonenumber:'17854170001',
-        signature:'whats up',
-        sex:'男',
-        region: ['上海市', '上海市', '黄浦区'],
-        avatar:'/image/avatar.png',
-        openid:null
+  globalData: {
+    username: 'User',
+    phonenumber: '17854170001',
+    signature: 'whats up',
+    sex: '男',
+    region: ['上海市', '上海市', '黄浦区'],
+    avatar: '/image/avatar.png',
+    openid: null
 
-    } ,
+  },
 
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
@@ -19,19 +19,17 @@ App({
     wx.getStorage({
       key: 'openid',
       success: function (res) {
-        getApp().globalData.openid=res.data,
-        console.log(res.data)
+        getApp().globalData.openid = res.data
         //this.setData({openid:res.data})
-       
       },
-      fail:function(){
+      fail: function () {
         wx.login({
 
           success: function (res) {
-            console.log(res.code);              
+            console.log(res.code);
 
             if (res.code) {
-              
+
               wx.request({
 
                 url: 'http://api.admination.cn/restful/getOpenid.php',
@@ -44,7 +42,7 @@ App({
 
                   // 登录成功
                   if (res.statusCode === 200) {
-                    getApp().globalData.openid = res.data;
+
                     console.log(res.data)// 服务器回包内容
                     wx.setStorage({
                       key: "openid",
@@ -60,54 +58,30 @@ App({
         });
       }
     })
-    wx.request({
-      url: 'http://api.admination.cn/restful/user/' + this.globalData.openid, //仅为示例，并非真实的接口地址
-      data: {
-
-      },
-      method: GET,
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: function (res) {
-        console.log(res.data)
-        getApp().globalData.username = res.data.nick,
-          getApp().globalData.phonenumber = res.data.phone,
-          getApp().globalData.sex = res.data.sex,
-          getApp().globalData.signature = res.data.signature,
-          getApp().globalData.region[0] = res.data.province,
-          getApp().globalData.region[1] = res.data.city,
-          getApp().globalData.region[2] = res.data.district
-
-      },
-      fail:function(){
-
-      }
-    })
-    
 
 
-    
+
+
   },
 
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
   onShow: function (options) {
-    
+
   },
 
   /**
    * 当小程序从前台进入后台，会触发 onHide
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
    */
   onError: function (msg) {
-    
+
   }
 })
