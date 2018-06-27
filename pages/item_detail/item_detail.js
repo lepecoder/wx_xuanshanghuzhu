af6548e09e28845285496a0da13b5a4560c42bab
CharlesLG7580 <459098348@qq.com>
2018-06-27 10:13:23 +0800

    test

--- pages/item_detail/item_detail.js
+++ pages/item_detail/item_detail.js
@@ -1,20 +1,94 @@
 // pages/item_detail/item_detail.js
+const app = getApp()
 Page({
+ 
+  data: {
+    height: 20,
+    focus: false,
+    ceshi: '',    
+    list: [{ }],
+    releaseFocus: false,
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  bindTextAreaBlur: function (e) {
+    this.setData({
+      ceshi: e.detail.value
+    })
+  },
+  RequestData: function () {
+    console.log("dsfasf")
+    var that = this;
+    wx.request({
+      url: 'https://api.admination.cn/restful/index.php/posts/1',
+      data: {},
+
+    success: function (res) {
+      
+    console.log(res.data)
+      },
+    fail:function(){
+      console.log('dsfasf')
+    }
+    })
+  },  
 
   /**
    * 页面的初始数据
    */
-  data: {
-  
-  },
+ 
+  add_after: function (e) {
+   
+    //要增加的数组
+    var newarray = [{
+      name: this.data.ceshi,
+    }];
 
+
+    this.setData({
+      list: this.data.list.concat(newarray)
+    });
+
+    
+  },
+  remove: function (e) {
+
+    var index = e.target.dataset.index
+    var id = e.currentTarget.dataset.id //获取下标
+    //通过`index`识别要删除第几条数据，第二个数据为要删除的项目数量，通常为1
+    this.data.list.splice(index, 1);
+    //渲染数据
+    this.setData({
+      list: this.data.list
+      
+    });
+    console.log("1111" + e.target.dataset);
+    console.log('44444:' + index);
+   
+
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
   },
-
+  /**
+  * 点击回复
+  */
+  bindReply: function (e) {
+    this.setData({
+      releaseFocus: true
+    })
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
