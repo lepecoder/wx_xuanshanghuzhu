196ac566f2a8092ba60829441fe834302dc303b2
clickTypeU <zsz_1997@163.com>
2018-06-29 01:07:05 +0800

    完成 我的信息

--- pages/profile_MyMessage/profile_MyMessage.js
+++ pages/profile_MyMessage/profile_MyMessage.js
@@ -5,13 +5,54 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    mymessage:[]
+  },
+
+//跳转至详情页
+  to_detail: function (e) {
+    //   console.log(e["currentTarget"]["id"]);
+    wx: wx.navigateTo({
+      url: '/pages/item_detail/item_detail?post_id=' + e["currentTarget"]["id"],
+      success: function (res) {
+        //console.log(res)
+      },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this
+    wx.request({
+      url: 'https://api.admination.cn/restful/getMyMessage.php?pid=' + getApp().globalData.openid,
+      data: {
+
+      },
+
+      header: {
+        'content-type':
+        'application/json'
+      },
+      success: function (res) {
+        console.log("回复-----")
+        console.log(res)
+        // if (res.statusCode == 404)   
+        //   res.data = []
+        var res_content = res.data;
+        res_content.forEach((item) => {
+          item.publish_time = item.publish_time.substring(5, 16)
+        });
+        that.setData({
+          mymessage: res_content
+        })
+      },
+      fail: function (res) {
+        console.log("MyMessage request fail");
+      }
+    })
   
   },
 
